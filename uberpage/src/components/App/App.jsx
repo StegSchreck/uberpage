f00eda9abcd381885db465c84efebe2b83693037
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-17 21:25:59 +0200

    multi-column layout depending on number of items

--- uberpage/src/components/App/App.jsx
+++ uberpage/src/components/App/App.jsx
@@ -4,21 +4,42 @@ import UberTile from '../UberTile/UberTile.jsx';
 import data from '../../data.json';
 
 function App() {
-  const itemHeight = 100 / (data.items.length + 1);
   const defaultItem = {
-      "title": "About this page",
-      "background_image": 'UberPage.png',
-      "link": "https://github.com/StegSchreck/uberpage",
-      "description": "This page was created using the UberPage project."
-    };
+    "title": "About this page",
+    "background_image": 'UberPage.png',
+    "link": "https://github.com/StegSchreck/uberpage",
+    "description": "This page was created using the UberPage project."
+  };
+  data.items.push(defaultItem);
+
+  // TODO 1 - handle odd number of items --> last items gets two columns width
+  // TODO 2 - set max number of columns depending on viewport width
+
+  function calculateNumberOfColumns() {
+    if (data.items.length <= 3) {
+      return 1;
+    }
+    if (data.items.length <= 8) {
+      return 2;
+    }
+    return 3;
+  }
+
+  const numberOfColumns = calculateNumberOfColumns();
+  const numberOfRows = Math.ceil(data.items.length / numberOfColumns);
+  const itemHeight = 100 / numberOfRows;
+  const itemWidth = 100 / numberOfColumns;
+  const gridStyle = { gridTemplateColumns: 'auto '.repeat(numberOfColumns) };
+
   return (
-    <div className="App">
+    <div className="App" style={gridStyle}>
       {
         data.items.map((item, index) => {
-          return <UberTile key={index} item={item} settings={data.settings} height={itemHeight}/>;
+          const gridColumn = index % numberOfColumns + 1;
+          const gridRow = Math.floor(index / numberOfColumns + 1);
+          return <UberTile key={index} item={item} settings={data.settings} height={itemHeight} width={itemWidth} gridColumn={gridColumn} gridRow={gridRow}/>;
         })
       }
-      <UberTile key='default' item={defaultItem} settings={data.settings} height={itemHeight}/>
     </div>
   );
 }
--- uberpage/src/components/UberTile/UberTile.jsx
+++ uberpage/src/components/UberTile/UberTile.jsx
@@ -6,11 +6,19 @@ class UberTile extends React.Component {
         const item = this.props.item;
         const settings = this.props.settings;
         const itemHeight = this.props.height;
+        const itemWidth = this.props.width || 100;
         const linkStyle = {};
         const tileStyle = {
             height: itemHeight + 'vh',
             minHeight: itemHeight + 'vh',
             maxHeight: itemHeight + 'vh',
+            width: itemWidth + 'vw',
+            minWidth: itemWidth + 'vw',
+            maxWidth: itemWidth + 'vw',
+            gridColumnStart: this.props.gridColumn,
+            gridColumnEnd: this.props.gridColumn,
+            gridRowStart: this.props.gridRow,
+            gridRowEnd: this.props.gridRow,
         };
         const backgroundImageStyle = {
             ...tileStyle,
@@ -18,15 +26,20 @@ class UberTile extends React.Component {
         const fontSize = itemHeight / 5;
         const textPosition = itemHeight / 2 + fontSize / 2;
         const textStyle = {
+            ...tileStyle,
             marginTop: `-${textPosition}vh`,
-            fontSize: `${fontSize}vh`
+            fontSize: `${fontSize}vh`,
         };
+
         if (item.background_color) {backgroundImageStyle['backgroundColor'] = item.background_color}
         if (item.text_color) {textStyle['color'] = item.text_color}
 
         if (item.background_image) {backgroundImageStyle['backgroundImage'] = 'url(/img/' + item.background_image + ')';}
         if (item.background_size) {backgroundImageStyle['backgroundSize'] = item.background_size;}
         if (item.title && item.background_image) {backgroundImageStyle['opacity'] = '0.2'}
+
+        // TODO 1 - handle max text width
+        // TODO 2 - show item description as overlay
         return (
           <div className="UberTile" style={tileStyle}>
               <a className="UberTile-link" href={item.link} title={item.description} target={settings.default_link_target} style={linkStyle}>
