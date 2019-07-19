877ace9e57dd35b986b81e07ae1d768fa1ca1238
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-19 20:54:29 +0200

    Fix lint findings in data.js

--- uberpage/src/data.js
+++ uberpage/src/data.js
@@ -1,23 +1,23 @@
 export default {
-  "items": [
+  items: [
     {
-      "link": "https://github.com/StegSchreck",
-      "text_color": "#ffffff",
-      "description": "My open source projects on GitHub",
-      "background_logo": "GitHub_white.png",
-      "background_color": "#000000"
+      link: 'https://github.com/StegSchreck',
+      text_color: '#ffffff',
+      description: 'My open source projects on GitHub',
+      background_logo: 'GitHub_white.png',
+      background_color: '#000000',
     },
     {
-      "link": "https://cv.schreck.berlin",
-      "description": "My Angular based online-CV",
-      "background_logo": "AngularCV_white_transparent.png",
-      "background_logo_size": "70%",
-      "background_picture": "cover.jpg",
-      "background_picture_size": "cover",
-      "background_color": "#ffffff"
-    }
+      link: 'https://cv.schreck.berlin',
+      description: 'My Angular based online-CV',
+      background_logo: 'AngularCV_white_transparent.png',
+      background_logo_size: '70%',
+      background_picture: 'cover.jpg',
+      background_picture_size: 'cover',
+      background_color: '#ffffff',
+    },
   ],
-  "settings": {
-    "default_link_target": "_blank"
-  }
+  settings: {
+    default_link_target: '_blank',
+  },
 };
