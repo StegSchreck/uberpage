d422f51be213f80fb46734c6eafcd504edccf895
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-20 20:32:08 +0200

    Let AWS version link to GitHub Page

--- uberpage/src/data.aws.js
+++ uberpage/src/data.aws.js
@@ -1,8 +1,8 @@
 export default {
   items: [
     {
-      link: 'https://github.com/StegSchreck',
-      text_color: '#ffffff',
+      link: 'https://stegschreck.github.io/',
+      text_color: '#FFFFFF',
       description: 'My open source projects on GitHub',
       background_logo: 'GitHub_white.svg',
       background_color: '#000000',
@@ -11,10 +11,10 @@ export default {
       link: 'https://cv.schreck.berlin',
       description: 'Angular based online-CV',
       background_logo: 'AngularCV_white_transparent.svg',
-      background_logo_size: '70%',
+      background_logo_size: 'contain',
       background_picture: 'cover.jpg',
       background_picture_size: 'cover',
-      background_color: '#ffffff',
+      background_color: '#FFFFFF',
     },
   ],
   settings: {
