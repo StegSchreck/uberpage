36ef79d4f0984dbd1ab09aefecd4fb29ffee8499
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2026-01-26 22:39:34 +0100

    Replace react-scripts with Vite

---
+++ uberpage/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+  build: {
+    outDir: 'build',
+  },
+});
